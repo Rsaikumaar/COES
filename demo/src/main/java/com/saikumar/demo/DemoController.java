b27e730b303cac9f8330a079c84ae9a2a4e344ec
Rokkam saikumar <137883882+Rsaikumaar@users.noreply.github.com>
2024-04-05 08:47:33 +0000

    commit

---
+++ demo/src/main/java/com/saikumar/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.saikumar.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hi kalyani";
+    }
+
+    @RequestMapping("/hello1")
+    public String Lock(){
+        return "Hi kalyani";
+    }
+}
